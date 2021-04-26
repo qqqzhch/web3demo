e51fe3535c7f16aabb7d74de3f41d7f3bcda3911
webfans <qqqzhch@163.com>
2021-04-26 10:16:08 +0800

    网络配置 (#317)

--- src/config/config.js
+++ src/config/config.js
@@ -3,35 +3,35 @@ export default {
   scUSDContractAddress: '0xe9d8a936bfb1a4dceccd8d97ed3c973399de303a',
   erc20Abi: require('../constants/abis/erc20.json'),
   netInfo: {
-    '1': {
-      imgSrc: require('../assets/img/eth48.png'),
-      name: 'Ethereum',
-      isBan: true
-    },
-    '3': {
-      imgSrc: require('../assets/img/eth48.png'),
-      name: 'Ropsten',
-      isBan: true
-    },
-    '4': {
-      imgSrc: require('../assets/img/eth48.png'),
-      name: 'Rinkeby',
-      isBan: false
-    },
-    '256': {
-      imgSrc: require('../assets/img/huobi48.svg'),
-      name: 'Heco Test',
-      isBan: true
-    },
-    '128': {
-      imgSrc: require('../assets/img/huobi48.svg'),
-      name: 'Heco Main',
-      isBan: true
-    },
+    // '1': {
+    //   imgSrc: require('../assets/img/eth48.png'),
+    //   name: 'Ethereum',
+    //   isBan: true
+    // },
+    // '3': {
+    //   imgSrc: require('../assets/img/eth48.png'),
+    //   name: 'Ropsten',
+    //   isBan: true
+    // },
+    // '4': {
+    //   imgSrc: require('../assets/img/eth48.png'),
+    //   name: 'Rinkeby',
+    //   isBan: false
+    // },
+    // '256': {
+    //   imgSrc: require('../assets/img/huobi48.svg'),
+    //   name: 'Heco Test',
+    //   isBan: true
+    // },
+    // '128': {
+    //   imgSrc: require('../assets/img/huobi48.svg'),
+    //   name: 'Heco Main',
+    //   isBan: true
+    // },
     '56': {
       imgSrc: require('../assets/img/binance48.svg'),
       name: 'BSC Main',
-      isBan: false
+      isBan: true
     },
     '97': {
       imgSrc: require('../assets/img/binance48.svg'),
